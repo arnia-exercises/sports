ffa6099c70fd205650d361fa346bc8be0795d939
Marcos Azevedo <marcos.workspace@gmail.com>
2023-08-18 21:54:13 -0300

    finish sports exercicies

---
+++ src/components/button/styled.ts
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const Button = styled.button`
+  background: #6950A1;
+  color: #fff;
+  padding: 8px 16px;
+  margin-block: 32px;
+  border-radius: 4px;
+  border: 0;
+  cursor: pointer;
+`
--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,15 +1,21 @@
+import { Button } from "../components/button/styled";
 import Template from "../components/template";
+import Thumb from "../components/thumb";
 import { NewItem } from "../types/news";
 
 type Props = {
   item: NewItem
+  backToNews: () => void
 }
 
-export default function Details ({ item }: Props) {
+export default function Details ({ item, backToNews }: Props) {
   return (
     <Template>
       <h1>{item.title}</h1>
       <p>{item.summary}</p>
+      <Thumb src={item.image} alt={item.title} />
+
+      <Button onClick={backToNews}>Voltar para as notícias</Button>
     </Template>
   )
 }
